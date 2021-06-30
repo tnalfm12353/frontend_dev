7c8c4f0eebd4b40184ef7eddf016bed8b83c8f8b
hong <tnalfm12353@naver.com>
2021-06-30 11:54:09 +0900

    javascript

--- ch05/src/main/webapp/tabbox.js
+++ ch05/src/main/webapp/tabbox.js
@@ -1,19 +1,19 @@
 var tabBox = {
     init: function(){
         console.log(this);
-        window.addEventListener("load",this.onWindowLoad.bind(this));
+        window.addEventListener("load",this._onWindowLoad.bind(this));
     },
-    onWindowLoad: function(){
+    _onWindowLoad: function(){
         console.log("!!!!");
         var divTabBox = document.getElementsByClassName("tab-box")[0];
         var ulTabs = divTabBox.childNodes[1];
         var liTabs = ulTabs.getElementsByTagName('li');
 
         for(var i = 0; i < liTabs.length; i++){
-            liTabs[i].addEventListener("click", this.onTabClicked);
+            liTabs[i].addEventListener("click", this._onTabClicked);
         }
     },
-    onTabClicked : function() {
+    _onTabClicked : function() {
         // unselected
         var lisSelected = document.getElementsByClassName("selected");
         (lisSelected.length == 1) && (lisSelected[0].className = "");
